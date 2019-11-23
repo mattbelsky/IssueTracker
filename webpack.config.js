e8d09ee230e0bc63afc83db8dc39f6afebfcc043
Matthew <matt.belsky@gmail.com>
2019-11-22 19:35:06 -0500

    configuration backup

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,43 @@
-var path = require('path');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
+    devtool: 'inline-source-map',
     cache: true,
     mode: 'development',
     output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        path: path.resolve(__dirname, 'src/main/resources/static/built'),
+        filename: 'bundle.js'
     },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
     module: {
         rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react'
+                            ]
+                        }
                     }
-                }]
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /(node_modules)/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
     }
-};
-
+}
